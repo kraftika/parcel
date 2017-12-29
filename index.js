0799325d1257f7963718c78fb95f7490a0efca0a
Ionut Pistol <ionut.pistol@gmail.com>
2017-12-29 16:58:53 +0200

    Parcel initial setup

---
+++ index.js
@@ -0,0 +1,11 @@
+import './styles.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = document.getElementById('app');
+
+const App = () => (
+    <div>React Apps</div>
+);
+
+ReactDOM.render(<App />, element);
